9fecb11293afa55afc4a2a6096a43ed2d8b3304b
jackiekazil <jackiekazil@gmail.com>
2011-10-04 01:27:02 -0400

    Adding choices files to the repo.

---
+++ egauge/core/choices.py
@@ -0,0 +1,9 @@
+ELECTION_TYPES = (
+    ('C', 'Caucus'),
+    ('O', 'Convention'),
+    ('E', 'Electoral College'),
+    ('G', 'General'),
+    ('N', 'Nomination'),
+    ('P', 'Primary'),
+    ('S', 'Special'),
+)
