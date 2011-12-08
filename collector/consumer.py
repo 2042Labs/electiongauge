08c04742bf08292c98ec5410b47b12798ca70326
Max <maxt@deepmile.com>
2011-12-08 14:46:51 -0500

    added data consumer and linguistic parser

--- collector/consumer.py
+++ collector/consumer.py
@@ -1,7 +1,21 @@
 import queue
+import simplejson as json
+import cunning_linguist as cl
+import sys
 
 def callback(data, message):
-    print data
+    js=json.loads(data)
+    #print js
+    try :
+        text=js['text']
+        print text
+        tokens=cl.process(text)
+        print tokens
+    except:
+        print ":(", sys.exc_info()
+        pass
+
+
 
 """
 To declare your own processor, either repurpose callback() or create another function just like that
@@ -10,3 +24,5 @@ and add it to the list that is the first argument to dequeue_loop().
 
 if __name__=="__main__":
     queue.dequeue_loop([callback])
+
+
