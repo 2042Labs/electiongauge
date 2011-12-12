d6d5f8f6609b63a591fb4db9485ecdef48cb88ec
Max <maxt@deepmile.com>
2011-12-12 14:19:59 -0500

    Added bag-of-words accumulation in a cooccurrence graph
    Added error handling in text processing

---
+++ collector/wordbag.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+wordbag.py
+
+Created by Maksim Tsvetovat on 2011-12-08.
+Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+import networkx as net
+
+class wordbag(object):
+
+    def __init__():
+        self.word_graph=net.DiGraph()
+        self.counter=0
+
+    def add_or_inc_edge(self,f,t):
+        """
+        Adds an edge to the graph IF the edge does not exist already. 
+        If it does exist, increment the edge weight.
+        Used for quick-and-dirty calculation of projected graphs from 2-mode networks.
+        """
+        g=self.word_graph
+        if g.has_edge(f,t):
+            g[f][t]['weight']+=1
+        else:
+            g.add_edge(f,t,weight=1)
+ 
+    def trim_edges(self, g, weight=1):
+        """
+        Remove edges with weights less then a threshold parameter ("weight")
+        """
+        g2=net.Graph()
+        for f, to, edata in g.edges(data=True):
+            if edata['weight'] > weight:
+                g2.add_edge(f,to,edata)
+        return g2       
+
+    def add_tokens(self,key, tokens):
+        if counter>1000:
+            self.prune()
+            self.save()
+            counter=0
+            
+        for token in tokens:
+            self.add_word(key,token)
+
+    def add_word(self,key,word):
+        word_graph.add_node(key, type='key')
+        word_graph.add_node(word, type='word')
+        self.add_or_inc_edge(word_graph,key,word)
+    
+    def prune(self):
+        self.word_graph=trim_edges(self.word_graph)
+    
+    def save(self):
+        print "<<<<<<< SAVING WORD-GRAPH >>>>>>>"
+        net.write_pajek(word_graph,"/tmp/egauge_wordgraph.net")
+    
+    def load(self):
+        self.word_graph=net.read_pajek("/tmp/egauge_wordgraph.net")
