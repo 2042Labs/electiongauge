b8bc3ec6693fa619ef80d31eabe25157cebb5e7a
Alex <alex@eat-up.org>
2011-12-12 19:06:12 +0000

    eventlet

--- collector/twitter.collector.py
+++ collector/twitter.collector.py
@@ -1,5 +1,10 @@
 import tweepy
 import queue
+try:
+    import eventlet
+    eventlet.monkey_patch()
+except:
+    pass
 
 username = "ElectionGauge"
 password = "cssgmu"
@@ -13,7 +18,7 @@ def stream_starter(enqueue):
     stream = tweepy.Stream(auth, listener=Listener())
     #stream.sample(async=True)
     stream.filter(track=open("candidates.txt").read().split(), follow=open("keywords.txt").read().split())
-    queue.LOG.info("Starting stream")
+    print("Starting stream")
 
 if __name__ == "__main__":
     queue.enqeue_loop(stream_starter)
