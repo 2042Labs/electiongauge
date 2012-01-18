ee1f270d90a710a2bda066c0922e4428ac165e06
Jackie Kazil <jackiekazil@gmail.com>
2012-01-18 00:57:59 +0000

    Commenting out python req for now, b/c issue w/ install.

--- scripts/s3_load.sh
+++ scripts/s3_load.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /root/electiongauge/collector
+cd /opt/egauge/electiongauge/collector
 /root/epd-7.1-2-rh5-x86/bin/python s3loader.py >> /var/log/s3loader.log 2>> /var/log/s3loader.log
 date >> /var/log/s3loader.log
