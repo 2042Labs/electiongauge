0b0bbbb82526e705e2c45a4e87865306378cb1a7
Max <maxt@deepmile.com>
2012-02-16 11:17:37 -0500

    Added a couple of visualization scripts

--- scripts/s3_load.sh
+++ scripts/s3_load.sh
@@ -2,4 +2,13 @@
 
 cd /opt/egauge/electiongauge/collector
 /root/epd-7.1-2-rh5-x86/bin/python s3loader.py >> /var/log/s3loader.log 2>> /var/log/s3loader.log
+
+cd /opt/egauge/electiongauge/visualizer
+/root/epd-7.1-2-rh5-x86/bin/python timeline.py >> /var/log/s3loader.log 2>> /var/log/s3loader.log
+/root/epd-7.1-2-rh5-x86/bin/python parallel_coords.py >> /var/log/s3loader.log 2>> /var/log/s3loader.log
+
+
+
 date >> /var/log/s3loader.log
+
+
---
+++ visualizer/parallel_coords.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import simplejson as json
+import random
+from collections import defaultdict
+
+    
+def plot_parallel(keys, gauges,name="gauges"): 
+    plt.clf()
+    data=defaultdict(list)
+    for k in sorted(keys):
+        for can in gauges[k]:
+            data[can].append(gauges[k][can])
+    
+    colors=['r','g','b','c','m','k','y'][:len(data.keys())]
+
+    i=0
+    for can in sorted(data.keys()):
+        plt.plot(range(len(keys)), data[can], colors[i],label=can)
+        plt.xticks(range(len(keys)), keys, size='small')
+        i+=1
+
+    plt.legend()
+    plt.savefig('../egauge/static_assets/data/'+name+'.png')
+
+
+def main():  
+    plt.ioff()
+    dataPath="../egauge/static_assets/data/"
+    jsonPath=dataPath+"json/"    
+
+    gauges=json.loads(open(jsonPath+'gauges.json','rb').read())
+
+    #keys=gauges.keys()[:10]
+    feb='AZ MI WA'.split(' ') 
+    superTuesday='AK GA ID MA ND OH OK TN VT VA'.split(' ')
+
+    plot_parallel(feb,gauges,name='feb')
+    plot_parallel(superTuesday,gauges,name='superTuesday')
+    
+
+
+if __name__ == '__main__':
+    main()
+    
+
+
+
+
+
