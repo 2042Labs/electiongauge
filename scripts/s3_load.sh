e9684a4af796627cb3d599004f19183c561fb736
Max <maxt@deepmile.com>
2012-02-16 11:19:08 -0500

    Added plotting to the cron job

--- scripts/s3_load.sh
+++ scripts/s3_load.sh
@@ -7,8 +7,6 @@ cd /opt/egauge/electiongauge/visualizer
 /root/epd-7.1-2-rh5-x86/bin/python timeline.py >> /var/log/s3loader.log 2>> /var/log/s3loader.log
 /root/epd-7.1-2-rh5-x86/bin/python parallel_coords.py >> /var/log/s3loader.log 2>> /var/log/s3loader.log
 
-
-
 date >> /var/log/s3loader.log
 
 
